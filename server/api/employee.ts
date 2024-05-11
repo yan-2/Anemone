4108898f7c37d65f333361b56200374f352fb333
yan-zhuang2 <yan.zhuang@mail.polimi.it>
2024-05-11 10:48:25 +0200

    feat:attach projs and services to the employee.

--- server/api/employee.ts
+++ server/api/employee.ts
@@ -4,13 +4,34 @@ export default defineEventHandler
 (async (
 ) => {
   const resp = await DB.from('employee').select('*')
-  // let data = resp.data.map(async (item: { id: any })=>{
-  //   // search the related projs
-  //   // console.log(item.id)
-  //   let serviceIDs = await DB.from('employee_services').select('*').eq('employee_id', item.id)
-  //   console.log(serviceIDs)
-  // })
+  let data =await Promise.all(resp.data.map(async (item: any)=>{
+    // Search the related services and projects
+    const relatedService = await DB.from('employee_services').select('*').eq('employee_id', item.id)
+    const relatedProj = await DB.from('employee_proj').select('*').eq('employee_id', item.id)
+    const serviceIDs = relatedService.data.map((item: any)=>{
+      return item.service_id
+    })
+    const projIDs = relatedProj.data.map((item: any)=>{
+      return item.proj_id
+    })
+    // Fetch detailed info for services and projects
+    let service = []
+    let project = []
+    if(serviceIDs.length){
+      let serviceResp = await DB.from('service').select('*').in('id', serviceIDs)
+      service = serviceResp.data
+    }
+    if(projIDs.length){
+      let projResp = await DB.from('project').select('*').in('id', projIDs)
+      project = projResp.data
+    }
+    return {
+      ...item,
+      service,
+      project
+    }
+  }))
   return {
-    data: resp.data,
+    data,
   }
 })

3092042b270e924b3b92dc7496a7c7ec228c67ca
AstroWLAN <dario2.crippa@mail.polimi.it>
2024-05-13 00:13:03 +0200

    Adds an initial version of the homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,22 @@
 <template>
-  <div>
-    <h1>Anemone</h1>
-    <b>Hello</b> there!
+  <div class="zStack">
+    <img id="logoBackground" src="~/public/img/logo_background.svg" alt="Background Logo">
+    <div id="splashInfo" class="container">
+      <div class="box">
+        <h1>Anemone</h1>
+        <div class="subtitle">
+          It's time to bloom again.
+        </div>
+        <div class="boxElement">
+          Hello stranger!<br>
+          Whether you're encountering challenges or seeking support, remember that you're not alone.<br>
+          Join us in our mission to empower women or let us help you.<br>
+        </div>
+        <button class="actionButton">
+          Request Help
+        </button>
+      </div>
+    </div>
+    <img id="logo" src="~/public/img/logo.svg" alt="Logo">
   </div>
 </template>

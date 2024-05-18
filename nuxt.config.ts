5b75deeee1e325ace22931bf644278e5882fab13
yan-zhuang2 <yan.zhuang@mail.polimi.it>
2024-05-18 11:04:04 +0200

    feat: remove bootstrap, add bootstrap-vue-next.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  components: { global: true },
-  devtools: { enabled: true },
-  ssr: true,
-  modules: [
-    // '@nuxtjs/stylelint-module',
-    '@nuxt/eslint',
-  ],
-  css: [
-    '~/assets/css/reset.css',
-    '~/assets/css/bootstrap.min.css',
-    '~/assets/css/foundations.less',
-    '~/assets/css/fonts.less',
-  ],
-  eslint: {
-    // checker: true, // <---
-    config: {
-      stylistic: true, // <---
+    components: {global: true},
+    devtools: {enabled: true},
+    ssr: true,
+    modules: [
+        '@bootstrap-vue-next/nuxt',
+        // '@nuxtjs/stylelint-module',
+        '@nuxt/eslint',
+    ],
+    css: [
+        '~/assets/css/reset.css',
+        'bootstrap/dist/css/bootstrap.min.css',
+        '~/assets/css/foundations.less',
+        '~/assets/css/fonts.less',
+    ],
+    eslint: {
+        // checker: true, // <---
+        config: {
+            stylistic: true, // <---
+        },
     },
-  },
 })

ad007131a9b6cb6bf09d981bf73d4f1ab49afd03
AstroWLAN <dario2.crippa@mail.polimi.it>
2024-05-28 21:43:13 +0200

    Fix an issue with the DOM

--- pages/index.vue
+++ pages/index.vue
@@ -9,20 +9,20 @@
 </template>
 
 <script setup lang="ts">
-// Imports components and variables
+import { onMounted } from 'vue'
 import { BACKGROUND_COLOR } from '~/assets/js/constants.js'
 import AnemoneFooter from '~/components/anemoneFooter.vue'
 import AnemoneNavbar from '~/components/anemoneNavbar.vue'
 
-// Set the background color of the document body
-document.body.style.backgroundColor = BACKGROUND_COLOR
+// Set the background color of the document body after the component is mounted
+onMounted(() => {
+  document.body.style.backgroundColor = BACKGROUND_COLOR
+})
 </script>
 
 <style scoped lang="less">
-// Variables
 @import "@/assets/css/variables.less";
 
-// Styling for the entire background
 .anemonePlaceholder {
   display: flex;
   flex-direction: column;

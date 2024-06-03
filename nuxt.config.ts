1f27a6b3375960fe39cfa2b55877bd3901041fb7
yan-2 <yan.zhuang@mail.polimi.it>
2024-06-03 10:31:47 +0200

    feat:add sitemap for SEO.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  site: {
+    url: 'https://anemone-ten.vercel.app',
+  },
   components: { global: true },
   devtools: { enabled: true },
   ssr: true,
-  modules: [
-    '@nuxt/eslint',
-  ],
+  modules: ['@nuxt/eslint', "@nuxtjs/sitemap"],
   css: [
     '~/assets/css/reset.css',
     '~/assets/css/foundations.less',

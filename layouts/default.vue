98fd99d54809463172852304133c7e7f5e53a216
AstroWLAN <dario2.crippa@mail.polimi.it>
2024-06-28 14:13:22 +0200

    Adds layouts

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex flex-col w-screen h-screen">
+    <!-- Navbar -->
+    <AnemoneNavbar />
+    <!-- Page content -->
+    <main class="flex flex-grow justify-center items-center">
+      <slot />
+    </main>
+    <!-- Footer -->
+    <AnemoneFooter />
+  </div>
+</template>
+
+<script setup lang="ts">
+// Imports components
+import AnemoneNavbar from '~/components/AnemoneNavbar.vue'
+import AnemoneFooter from '~/components/AnemoneFooter.vue'
+</script>

5f3bb3f6faa7c6070b4c28f7610a6adef4a157e5
AstroWLAN <dario2.crippa@mail.polimi.it>
2024-06-28 14:12:09 +0200

    Updates index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,52 +1,43 @@
 <template>
-  <div class="flex justify-center items-center">
-    <div class="relative max-w-full max-h-full grid-cols-1 grid-rows-1 justify-center items-center">
-      <div class="absolute inset-0 z-0 flex items-center justify-center">
-        <img
-          src="~/public/img/vectors/logoBackground.svg"
-          alt="Background shape"
-          class="w-full md:w-3/4 lg:w-2/5 h-auto lg:-translate-y-36 rotate-6"
-        >
-      </div>
-      <div class="flex flex-col items-center justify-center">
-        <!-- Woman vector -->
-        <img
-          src="~/public/img/vectors/logo.svg"
-          alt="The website logo"
-          class="w-full md:w-3/4 lg:w-2/5 h-auto lg:translate-y-2 z-20"
-        >
-        <!-- Splash information box -->
-        <div class="bg-neutral border border-primary p-6 max-w-2xl z-10">
-          <div class="text-center">
-            <div class="font-rosamila text-big text-primary">
-              Anemone
-            </div>
-            <div class="font-roboto text-secondary text-base">
-              It's time to bloom again
-            </div>
-            <p class="text-primary font-roboto text-base mb-4">
-              Hello stranger!<br>
-              Whether you're encountering challenges or seeking support,
-              remember that you're not alone.<br>
-              Join us in our mission to empower women or let us help you.
-            </p>
-            <!-- Request help button -->
-            <div class="text-center">
-              <button class="font-roboto bg-action text-neutral px-4 py-2 text-sm transition-colors hover:bg-opacity-90">
-                Request Help
-              </button>
-            </div>
-          </div>
-        </div>
+  <div class="relative grid grid-cols-1 grid-rows-1 justify-center items-center px-8 text-center">
+    <!-- Background blob -->
+    <div class="absolute inset-x-0 top-0 flex items-center justify-center z-0">
+      <img
+        src="~/public/img/vectors/shape.svg"
+        alt="Background shape"
+        class="min-w-2/5"
+      >
+    </div>
+    <div class="flex flex-col items-center justify-center">
+      <!-- Woman image -->
+      <img
+        src="~/public/img/vectors/woman.svg"
+        alt="Website testimonial"
+        class="min-w-2/3 translate-y-2 z-20"
+      >
+      <!-- Splash information box -->
+      <div class="bg-neutral border border-primary max-w-xl p-4 rounded-lg z-10">
+        <h2 class="font-rosamila text-5xl text-primary">
+          Anemone
+        </h2>
+        <p class="text-secondary mb-4">
+          It's time to bloom again
+        </p>
+        <p class="text-primary mb-8">
+          Hello stranger!<br>
+          Whether you're encountering challenges or seeking support,
+          remember that you are not alone.<br>
+          Join us in our mission to empower women or let us help you.
+        </p>
+        <RequestButton />
       </div>
     </div>
   </div>
 </template>
 
-<script>
-
+<script setup>
+// Defines the layout used
+import RequestButton from '~/components/RequestButton.vue'
+// Declares the adopted layout
+definePageMeta({ layout: 'default' })
 </script>
-
-<style>
-/* Add any additional styles here if needed */
-</style>

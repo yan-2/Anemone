7d4b63df7d56d26d1869573ae1ada2a4e484207c
AstroWLAN <dario2.crippa@mail.polimi.it>
2024-06-28 14:19:26 +0200

    Adds the request button component

---
+++ components/RequestButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <!-- Request help button -->
+  <button
+    class="hover:bg-action/85"
+    :class="[
+      'bg-action text-neutral px-4 py-2 text-sm rounded-md',
+      'transition-all duration-200 ease-in-out',
+      { 'transform scale-95': isClicked },
+    ]"
+    @click="handleClick"
+  >
+    Request Help
+  </button>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const isClicked = ref(false)
+
+// Exports component
+defineOptions({ name: 'RequestButton' })
+
+// Opens the email client on the local machine
+const handleClick = () => {
+  isClicked.value = true
+
+  // Resets the button size after a small delay
+  setTimeout(() => {
+    isClicked.value = false
+  }, 200)
+
+  const email = 'anemergency@anemone.it'
+  const subject = encodeURIComponent('Help Request')
+  window.location.href = `mailto:${email}?subject=${subject}`
+}
+</script>

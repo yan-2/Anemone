ea306cb5c02d5f91ac2a39ec2304398b331412a2
AstroWLAN <dario2.crippa@mail.polimi.it>
2024-06-28 18:52:34 +0200

    Updates ServiceMenu

--- components/ServicesMenu.vue
+++ components/ServicesMenu.vue
@@ -1,26 +1,31 @@
 <template>
   <div class="relative group">
     <NuxtLink
-        to="/services"
-        class="hover:text-primary dark:hover:text-primary-dark"
-        @mouseenter="openMenu"
-        @mouseleave="startCloseTimer"
+      to="/services"
+      class="hover:text-primary dark:hover:text-primary-dark"
+      @mouseenter="openMenu"
+      @mouseleave="startCloseTimer"
     >
       Services
     </NuxtLink>
     <div
-        v-if="isOpen"
-        class="absolute left-1/2 transform -translate-x-1/2 mt-2 w-56 rounded-md shadow-lg bg-white dark:bg-gray-800 ring-1 ring-black ring-opacity-5 focus:outline-none"
-        @mouseenter="cancelCloseTimer"
-        @mouseleave="closeMenu"
+      v-if="isOpen"
+      class="absolute left-1/2 transform -translate-x-1/2 mt-4 w-sm rounded-md border border-primary bg-neutral"
+      @mouseenter="cancelCloseTimer"
+      @mouseleave="closeMenu"
     >
-      <div class="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+      <div
+        class="py-1"
+        role="menu"
+        aria-orientation="vertical"
+        aria-labelledby="options-menu"
+      >
         <NuxtLink
-            v-for="service in services"
-            :key="service.id"
-            :to="`/services/${service.id}`"
-            class="block px-4 py-2 text-sm text-left text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
-            role="menuitem"
+          v-for="service in services"
+          :key="service.id"
+          :to="`/services/${service.id}`"
+          class="block px-4 py-2 text-left text-secondary hover:text-primary"
+          role="menuitem"
         >
           {{ service.name }}
         </NuxtLink>
@@ -33,11 +38,11 @@
 import { ref, onMounted, onBeforeUnmount } from 'vue'
 
 interface Service {
-  id: number;
-  name: string;
-  description: string;
-  availability: string;
-  testimonial: string;
+  id: number
+  name: string
+  description: string
+  availability: string
+  testimonial: string
 }
 
 const isOpen = ref(false)
@@ -71,7 +76,8 @@ const fetchServices = async () => {
     if (data.value) {
       services.value = data.value.data
     }
-  } catch (error) {
+  }
+  catch (error) {
     console.error('Error fetching services:', error)
   }
 }
